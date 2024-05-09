23d8ff54e2a2fadc2dfeee83376c88f9d3075ede
andreescu-anca-30127 <99620657+andreescu-anca-30127@users.noreply.github.com>
2024-05-09 17:32:30 +0300

    codd

--- main.py
+++ main.py
@@ -19,6 +19,9 @@ from sklearn.linear_model import LinearRegression
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_squared_error
+from ucimlrepo import fetch_ucirepo
+
+# fetch dataset
 breast_cancer_wisconsin_prognostic = fetch_ucirepo(id=16)
 # data (as pandas dataframes)
 X = breast_cancer_wisconsin_prognostic.data.features
@@ -61,21 +64,22 @@ Y_test = sc_Y.transform(Y_test)
 # Crearea rețelei neuronale
 def ANN(Y_train, output, batch, epochs, error,num_layers,num_neurons):
     classifier = Sequential()
+
     # Adăugarea straturilor ascunse
     for i in range(num_layers):
         # Primul strat ascuns trebuie să aibă dimensiunea de intrare specificată
         if i == 0:
             classifier.add(Dense(input_dim= 30, units=num_neurons, activation="relu", kernel_initializer="uniform"))
-            classifier.add(Dropout(0.3))
+
         else:
             classifier.add(Dense(units=num_neurons, activation="relu", kernel_initializer="uniform"))
-            classifier.add(Dropout(0.3))
+
 
 
 
     classifier.add(Dense(output))
     classifier.summary()
-    create_optimizer = Adam(learning_rate=0.4e-3, epsilon = 1e-08)
+    create_optimizer = Adam(learning_rate=0.04e-4, epsilon = 1e-08)
     classifier.compile(optimizer= create_optimizer, loss=error, metrics=['accuracy'])
     classifier.fit(X_train, Y_train, batch_size=batch, epochs=epochs,validation_split=0.1)
 
@@ -84,7 +88,7 @@ def ANN(Y_train, output, batch, epochs, error,num_layers,num_neurons):
     bias = classifier.layers[0].get_weights()[1]
     weights = classifier.layers[0].get_weights()[0]
     return yhat, bias, weights
-yhat, bias, weights = ANN(Y_train, output=1, batch=5, epochs=100, error='mse',num_layers=1,num_neurons=23)
+yhat, bias, weights = ANN(Y_train, output=1, batch=5, epochs=150, error='mse',num_layers=1,num_neurons=23)
 
 # # Definirea listelor de valori posibile pentru parametrii căutați
 # best_mse = float('inf')
@@ -113,8 +117,6 @@ yhat, bias, weights = ANN(Y_train, output=1, batch=5, epochs=100, error='mse',nu
 #
 # print("Best parameters:", best_params)
 # print("Best MSE:", best_mse)
-
-
 # Evaluarea vizuală
 plt.plot(Y_test, 'red', label='Real Output')
 plt.plot(yhat, 'green', label='Predicted Output')
@@ -123,7 +125,6 @@ plt.xlabel('Number of samples')
 plt.ylabel('Measured value')
 plt.legend()
 plt.show()
-
 
 # Calcularea erorilor
 mse = np.square(np.subtract(Y_test, yhat)).mean()
